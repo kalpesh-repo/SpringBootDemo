d9e34b0543c2336ab77eac5fff1a74a53e7f85d9
kalpesh <kalpeshneve@gmail.com>
2020-11-10 13:30:34 +0530

    changes added

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is a simple example of Simple Starter Project using GIT Repository
 As stated, this is just introduction of Starter We Project
 
 ## Purpose
+The purpose for this is to provide simple demo for GIT Basics
 
 ## Deployment 
 
--- src/main/java/com/example/demo/MyControllerDemo.java
+++ src/main/java/com/example/demo/MyControllerDemo.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Controller
 @RestController
 public class MyControllerDemo {
 	@GetMapping("/test")
